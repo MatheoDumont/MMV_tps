71a693b8dc5b0dffc0f8a31f529a7a8f081d94ce
Jean-Baptiste Loutfalla <jb.loutfalla@orange.fr>
2020-11-30 14:08:51 +0100

    Update vec2

--- tp2/include/vec.h
+++ tp2/include/vec.h
@@ -10,6 +10,7 @@ struct vec3
     double z;
 
     vec3() : x(0), y(0), z(0) {}
+    vec3(const vec3& v) : x(v.x), y(v.y), z(v.z) {}
 
     vec3(float x_, float y_, float z_) : x(x_), y(y_), z(z_) {}
     vec3(float x_, float y_) : x(x_), y(y_), z(0.) {}
@@ -22,25 +23,51 @@ struct vec3
 //     return vec3(x_, y_), 0.;
 // }
 
-typedef vec3 vec2;
+struct vec2
+{
+    double x;
+    double y;
+
+    vec2() : x(0), y(0) {}
+    vec2(const vec2& v) : x(v.x), y(v.y) {}
+
+    vec2(float x_, float y_) : x(x_), y(y_) {}
+
+    bool operator==(const vec2 &o) const;
+};
 
 vec3 operator-(const vec3 &p1, const vec3 &p2);
+vec2 operator-(const vec2 &p1, const vec2 &p2);
 
 vec3 operator*(const vec3 &p1, const vec3 &p2);
 vec3 operator*(float k, const vec3 &p);
 vec3 operator*(const vec3 &p, float k);
 
+vec2 operator*(const vec2 &p1, const vec2 &p2);
+vec2 operator*(float k, const vec2 &p);
+vec2 operator*(const vec2 &p, float k);
+
 vec3 operator/(const vec3 &p1, const vec3 &p2);
 vec3 operator/(const vec3 &p, float k);
+
+vec2 operator/(const vec2 &p1, const vec2 &p2);
+vec2 operator/(const vec2 &p, float k);
 // vec3 operator/(float k, const vec3 &p);
 
 vec3 operator+(const vec3 &p1, const vec3 &p2);
+
+vec2 operator+(const vec2 &p1, const vec2 &p2);
 
 float norm(const vec3 &p);
 vec3 normalize(const vec3 &p);
 float dot(const vec3 &p1, const vec3 &p2);
 vec3 cross(const vec3 &p1, const vec3 &p2);
 
+float norm(const vec2 &p);
+vec2 normalize(const vec2 &p);
+float dot(const vec2 &p1, const vec2 &p2);
+
 std::ostream &operator<<(std::ostream &o, const vec3 &p);
+std::ostream &operator<<(std::ostream &o, const vec2 &p);
 
 #endif
--- tp2/src/vec.cpp
+++ tp2/src/vec.cpp
@@ -6,15 +6,31 @@ bool vec3::operator==(const vec3 &v) const
     return (x == v.x) && (y == v.y) && (z == v.z);
 }
 
+bool vec2::operator==(const vec2 &v) const
+{
+    return (x == v.x) && (y == v.y);
+}
+
 std::ostream &operator<<(std::ostream &o, const vec3 &v)
 {
     o << "(" << v.x << ", " << v.y << ", " << v.z << ")";
     return o;
 }
 
+std::ostream &operator<<(std::ostream &o, const vec2 &v)
+{
+    o << "(" << v.x << ", " << v.y << ")";
+    return o;
+}
+
 vec3 operator-(const vec3 &v1, const vec3 &v2)
 {
     return vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+vec2 operator-(const vec2 &v1, const vec2 &v2)
+{
+    return vec2(v1.x - v2.x, v1.y - v2.y);
 }
 
 vec3 operator*(const vec3 &v1, const vec3 &v2)
@@ -32,6 +48,21 @@ vec3 operator*(float k, const vec3 &v)
     return v * k;
 }
 
+vec2 operator*(const vec2 &v1, const vec2 &v2)
+{
+    return vec2(v1.x * v2.x, v1.y * v2.y);
+}
+
+vec2 operator*(const vec2 &v, float k)
+{
+    return vec2(v.x * k, v.y * k);
+}
+
+vec2 operator*(float k, const vec2 &v)
+{
+    return v * k;
+}
+
 vec3 operator/(const vec3 &v1, const vec3 &v2)
 {
     return vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
@@ -42,9 +73,24 @@ vec3 operator/(const vec3 &v, float k)
     return vec3(v.x / k, v.y / k, v.z / k);
 }
 
+vec2 operator/(const vec2 &v1, const vec2 &v2)
+{
+    return vec2(v1.x / v2.x, v1.y / v2.y);
+}
+
+vec2 operator/(const vec2 &v, float k)
+{
+    return vec2(v.x / k, v.y / k);
+}
+
 vec3 operator+(const vec3 &v1, const vec3 &v2)
 {
     return vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+vec2 operator+(const vec2 &v1, const vec2 &v2)
+{
+    return vec2(v1.x + v2.x, v1.y + v2.y);
 }
 
 float length(const vec3 &v)
@@ -54,7 +100,18 @@ float length(const vec3 &v)
                 (v.z * v.z));
 }
 
+float length(const vec2 &v)
+{
+    return sqrt((v.x * v.x) +
+                (v.y * v.y));
+}
+
 vec3 normalize(const vec3 &v)
+{
+    return v / length(v);
+}
+
+vec2 normalize(const vec2 &v)
 {
     return v / length(v);
 }
@@ -62,6 +119,11 @@ vec3 normalize(const vec3 &v)
 float dot(const vec3 &v1, const vec3 &v2)
 {
     return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+}
+
+float dot(const vec2 &v1, const vec2 &v2)
+{
+    return (v1.x * v2.x) + (v1.y * v2.y);
 }
 
 vec3 cross(const vec3 &v1, const vec3 &v2)
@@ -72,3 +134,4 @@ vec3 cross(const vec3 &v1, const vec3 &v2)
     v.z = (v1.x * v2.y) - (v1.y * v2.x);
     return v;
 }
+
