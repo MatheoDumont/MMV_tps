6ad240563d1917f9561f590179dff555566dcdda
Matheo <math.dumont.md@gmail.com>
2020-12-13 14:47:33 +0100

    rebase

--- tp2/shaders/base.fs.glsl
+++ tp2/shaders/base.fs.glsl
@@ -12,7 +12,7 @@ out vec4 frag_color;
 const float K = 0.9;
 const float PI = 3.14159265359;
 const float GAMMA = 1.0 / 2.2;
-
+const float shininess = 16.0;
 void main()
 {
     vec3 o = normalize(u_camera - v_position);
@@ -20,8 +20,12 @@ void main()
 
     float cos_theta = dot(normalize(v_normal), l);
     float fr = K / PI;
-  
-    vec3 color = fr * v_color * cos_theta;
+
+    vec3 halfdir = normalize(l + o);
+    float specangle = max(dot(halfdir, v_normal), 0.0);
+    float specular = (1 - K) * ((shininess * 8.0) / (8.0 * PI)) * pow(specangle, shininess);
+    vec3 color = cos_theta * (fr * v_color + specular * vec3(0.5, 0.5, 0.5));
+   
     color = pow(color, vec3(GAMMA));
     
     frag_color = vec4(color, 1.f);
